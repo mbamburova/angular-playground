fee892adf2854aabe57f27ea34fba771269c8ef6
Michaela Bamburová <433732@mail.muni.cz>
2017-03-31 12:41:30 +0200

    passing ingredients from recipes via services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,39 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Zucchini Corn Dalia Recipe', 'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/5/51/Zucchini-corn-dalia-recipe.jpg'),
+    new Recipe(
+      'Zucchini Corn Dalia Recipe',
+      'Yumyyy!!!',
+      'https://upload.wikimedia.org/wikipedia/commons/5/51/Zucchini-corn-dalia-recipe.jpg',
+    [
+      new Ingredient('Pasta', 1),
+      new Ingredient('Corn', 3)
+    ]),
 
-    new Recipe('Vermicelli Recipe', 'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Vermicelli-upma-recipe.jpg')
+    new Recipe(
+      'Vermicelli Recipe',
+      'Delicious recipe :)',
+      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Vermicelli-upma-recipe.jpg',
+    [
+      new Ingredient('Vermicelli pasta', 2),
+      new Ingredient('carrot', 4)
+    ])
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice(); // return a new array a copy
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
