4cd156b3289fc7885a488bbd6719193d4decf5e9
Michaela Bamburová <433732@mail.muni.cz>
2017-04-01 16:15:42 +0200

    added child routing, passing dynamic params to links

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -33,6 +33,10 @@ export class RecipeService {
     return this.recipes.slice(); // return a new array a copy
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
